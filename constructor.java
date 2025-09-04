fae88dc7fdea79bf947b77faa27b087e6365bee7
DarkFox90 <carlospozuelos3@gmail.com>
2025-09-04 13:59:46 -0600

    cree archivo prueba.java y  constructor.java

---
+++ constructor.java
@@ -0,0 +1,17 @@
+public class constructor {
+    private String nombre;
+    private int edad;
+
+    public constructor(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+}
